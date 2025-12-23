e6f81fa5ff632e5294da6693a72a3beac9fe3c42
vrinda-1234 <163041090+vrinda-1234@users.noreply.github.com>
2025-12-23 17:11:38 +0530

    Initial setup: routing, Home and Login pages

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+function Login() {
+    return (
+      <div>
+        <h2>Login to SafeHer</h2>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
