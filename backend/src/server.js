e462e7b78bdba67d7d857632ef8037c7d06bc58e
vrinda-1234 <163041090+vrinda-1234@users.noreply.github.com>
2026-01-18 02:14:41 +0530

    Add backend setup with Express

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+console.log("ENV PORT:", process.env.PORT);
+connectDB();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("SafeHer backend running 🚀");
+})
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
